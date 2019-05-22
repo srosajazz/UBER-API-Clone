6d36223d7e09469b46ace0c7937c7271cd5e5dd9
Sergio Rosa <scarlosjazz@gmail.com>
2019-05-22 18:15:06 -0400

    Creating UBER api interface with React Native

---
+++ Uber/src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import MapView from "react-native-maps";
+
+import { View } from "react-native";
+
+const App = () => (
+  <View style={{ flex: 1 }}>
+    <MapView
+      style={{ flex: 1 }}
+      region={{
+        latitude: 37.785834,
+        longitude: -122.406417,
+        latitudeDelta: 0.0143,
+        longitudeDelta: 0.0134
+      }}
+      showsUserLocation
+      loadingEnabled
+    />
+  </View>
+);
+
+export default App;
